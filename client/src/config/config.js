a22244bd53504ec9ce17631eb8043cf614e34ac1
Online-Assessment-System <120499092+Online-Assessment-System@users.noreply.github.com>
2022-12-13 23:25:25 +0530

    Update config.js

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,4 +1,4 @@
 const CLIENT_URL = process.env.REACT_APP_CLIENT_URL || "http://localhost:3000";
-const SERVER_URL = process.env.REACT_APP_SERVER_URL || "http://localhost:5000";
+const SERVER_URL = process.env.REACT_APP_SERVER_URL || "https://online-assessment-system-server.onrender.com";
 
 module.exports = { CLIENT_URL, SERVER_URL };
